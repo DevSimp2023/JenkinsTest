99d25ca65502b87ccde173bf157682af438a917b
DEV01\Dev001 <simpleDev2023@proton.me>
2023-04-12 21:04:39 +0800

    no message

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -8,7 +8,7 @@ using UnityEditor.Build.Reporting;
 // ------------------------------------------------------------------------
 // https://docs.unity3d.com/Manual/CommandLineArguments.html
 // ------------------------------------------------------------------------
-public class JenkinsBuild {
+public class JenkinsPipeline {
     private static readonly string[] EnabledScenes = FindEnabledEditorScenes();
   
     // ------------------------------------------------------------------------
